849f56b0993259fc1924a705e927b6e12171aa63
Maduabuchi Okonkwo <maduabuchiokonkwo@gmail.com>
2018-09-01 21:59:38 +0100

    added bootstrap, jobs and technologies

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from technologies.models import Technologies
+from .models import Job
 # Create your views here.
+
+
+
+def home(request):
+    jobs = Job.objects
+    technologies = Technologies.objects
+    return render(request, 'jobs/home.html', {'jobs': jobs, 'technologies': technologies})
--- technologies/models.py
+++ technologies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+class Technologies(models.Model):
+    Title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/technologies/')
+    
